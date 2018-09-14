edaa7996e732f64dbeeb196126074f06a419771f
Billy Chan <billy.chan@dowjones.com>
2018-09-14 14:56:43 +0800

    Added Hex Int UIColor init

---
+++ NMSwiftKit/NMSwiftKit/UIColor+HexInt.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+HexInt.swift
+//  NMSwiftKit
+//
+//  Created by Billy Chan on 9/14/18.
+//  Copyright © 2018 Billy Chan. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: Int32, alpha: Int8 = Int8(0xFF)) {
+        var mutableHex = hex
+        
+        let blue = CGFloat(mutableHex & 0xFF) / 255.0
+        mutableHex = mutableHex >> 8
+        
+        let green = CGFloat(mutableHex & 0xFF) / 255.0
+        mutableHex = mutableHex >> 8
+        
+        let red = CGFloat(mutableHex & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: CGFloat(alpha) / 255.0)
+    }
+}
