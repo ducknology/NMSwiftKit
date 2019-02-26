44cb3d97c2af2049cc9046e066516fbfdcbd8803
Billy Chan <billy.chan@dowjones.com>
2019-02-26 15:32:18 +0800

    Better indication of returned constraints

--- NMSwiftKit/NMSwiftKit/UIViewHelper.swift
+++ NMSwiftKit/NMSwiftKit/UIViewHelper.swift
@@ -9,28 +9,71 @@
 import UIKit
 
 public extension UIView {
-    @discardableResult public func addSubviewAndExpand(_ subview: UIView) -> [NSLayoutConstraint] {
+    @discardableResult public func addSubviewAndExpand(_ subview: UIView) -> SideConstrains? {
         self.addSubview(subview)
         
         return subview.expandToMatchSuperview()
     }
     
-    @discardableResult public func expandToMatchSuperview() -> [NSLayoutConstraint] {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        
-        var constraints = [NSLayoutConstraint]()
-        
+    @discardableResult public func expandToMatchSuperview() -> SideConstrains? {
         guard let superview = self.superview else {
-            return constraints
+            return nil
         }
         
-        constraints.append(self.leftAnchor.constraint(equalTo: superview.leftAnchor))
-        constraints.append(self.topAnchor.constraint(equalTo: superview.topAnchor))
-        constraints.append(self.rightAnchor.constraint(equalTo: superview.rightAnchor))
-        constraints.append(self.bottomAnchor.constraint(equalTo: superview.bottomAnchor))
+        let constraints = SideConstrains(top: self.leftAnchor.constraint(equalTo: superview.leftAnchor),
+                                         left: self.topAnchor.constraint(equalTo: superview.topAnchor),
+                                         right: self.rightAnchor.constraint(equalTo: superview.rightAnchor),
+                                         bottom: self.bottomAnchor.constraint(equalTo: superview.bottomAnchor))
         
-        self.superview?.addConstraints(constraints)
+        constraints.all.forEach { $0.isActive = true }
         
         return constraints
+    }
+    
+    @discardableResult public func attachViewAtBottom(_ subview: UIView,
+                                                      _ height: CGFloat,
+                                                      constraintPiorities: UILayoutPriority = .required) -> BottomAttachConstraints? {
+        //  Make sure they are under the same parent
+        guard let superview = self.superview else {
+            return nil
+        }
+        
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        
+        superview.addSubview(subview)
+        
+        let constraints = BottomAttachConstraints(top: subview.topAnchor.constraint(equalTo: self.bottomAnchor),
+                                                  left: subview.leftAnchor.constraint(equalTo: superview.leftAnchor),
+                                                  right: subview.rightAnchor.constraint(equalTo: superview.rightAnchor),
+                                                  height: subview.heightAnchor.constraint(equalToConstant: height))
+        
+        constraints.all.forEach { constraint in
+            constraint.priority = constraintPiorities
+            constraint.isActive = true
+        }
+        
+        return constraints
+    }
+}
+
+public struct BottomAttachConstraints {
+    let top: NSLayoutConstraint
+    let left: NSLayoutConstraint
+    let right: NSLayoutConstraint
+    let height: NSLayoutConstraint
+    
+    var all: [NSLayoutConstraint] {
+        return [self.top, self.left, self.right, self.height]
+    }
+}
+
+public struct SideConstrains {
+    let top: NSLayoutConstraint
+    let left: NSLayoutConstraint
+    let right: NSLayoutConstraint
+    let bottom: NSLayoutConstraint
+    
+    var all: [NSLayoutConstraint] {
+        return [self.top, self.left, self.right, self.bottom]
     }
 }
